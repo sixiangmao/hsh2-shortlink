10de64f138cc447026df66c26e4f4afff2f87f41
sixiangmao <3032342967@qq.com>
2026-03-31 17:27:20 +0800

    短链接项目完成

---
+++ admin/src/main/java/com/example/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,18 @@
+package com.example.shortlink.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients("com.example.shortlink.admin.remote")
+@MapperScan("com.example.shortlink.admin.dao.mapper")
+public class ShortLinkAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
